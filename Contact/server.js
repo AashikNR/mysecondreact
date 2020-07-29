571d74d0694f12f047ea55381b2dbbee94db40c5
AashikNR <ashiknizam77@gmail.com>
2020-07-29 10:31:59 +0530

    with context api

---
+++ Contact/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const path = require("path");
+const app = express();
+connectDB();
+app.use(express.json({ extended: false }));
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
